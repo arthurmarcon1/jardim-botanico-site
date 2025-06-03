0e1f61babe8ce25653b95481aba70ee4733395c6
Argschadt <110493607+Argschadt@users.noreply.github.com>
2025-06-03 11:43:20 -0300

    TelhadoVerde melhoria

--- frontend/src/pages/TelhadoVerde.js
+++ frontend/src/pages/TelhadoVerde.js
@@ -3,18 +3,7 @@ import SobreTelhadoVerde from "../components/sobre/SobreTelhadoVerde";
 
 function TelhadoVerde() {
   return (
-    <>
     <SobreTelhadoVerde></SobreTelhadoVerde>
-    <div style={{ height: "100vh", backgroundColor: "#c3debf" }}>
-      <iframe
-        src="buildNova/index.html" // olhar o nome da build para evitar erros !!!!!!!!!!!!!!
-        title="Telhado Verde"
-        width="100%"
-        height="100%"
-        style={{ border: 'none', marginTop: '40px' }}
-      ></iframe>
-      </div>
-    </>
   );
 }
 
