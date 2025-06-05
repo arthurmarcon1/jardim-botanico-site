b2c242191b10df78f6efbce4b08601c90dbdadad
Argschadt <110493607+Argschadt@users.noreply.github.com>
2025-06-05 10:27:01 -0300

    arrumar navbar para mobile

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa o JS do Bootstrap para funcionalidade do navbar
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
